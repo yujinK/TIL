c3fcaadf0d0b2ed1c3b02f2b182d4a18e05c1a16
yujinK <yujinkim.dev@gmail.com>
2019-03-15 22:36:18 +0900

    Codility Lession3-2

---
+++ Algorithm/Codility/Lession3_2.java
@@ -0,0 +1,26 @@
+package codility;
+
+public class Lession3_2 {
+
+	public static void main(String[] args) {
+		int[] A = {1, 2, 3, 5};
+		System.out.println(permMissingElem(A));
+	}
+
+	public static int permMissingElem(int[] A) {
+		int sum = 0;
+		int N = A.length + 1;
+		
+		if (N%2 == 0) {
+			sum = (1+N) * (N/2);
+		} else {
+			sum = (N * (N/2)) + N;
+		}
+		
+		for (int i : A) {
+			sum -= i;
+		}
+		
+		return sum;
+	}
+}
