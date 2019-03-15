5581874e3795724e775998cdc545a4c16b4caea7
yujinK <yujinkim.dev@gmail.com>
2019-03-15 22:17:14 +0900

    Codility Lession3-1

---
+++ Algorithm/Codility/Lession3_1.java
@@ -0,0 +1,31 @@
+package codility;
+
+public class Lession3_1 {
+
+	public static void main(String[] args) {
+		System.out.println(frogJump(5, 105, 3));
+	}
+	
+	public static int frogJump(int X, int Y, int D) {
+		int result = 0;
+		
+		if ((Y-X)%D == 0) {
+			result = (Y-X)/D;
+		} else {
+			result = ((Y-X)/D) + 1;
+		}
+		
+		return result;
+	}
+	
+//	public static int frogJump(int X, int Y, int D) {
+//		int result = 0;
+//		
+//		while (X < Y) {
+//			X += D;
+//			result++;
+//		}
+//		
+//		return result;
+//	}
+}
