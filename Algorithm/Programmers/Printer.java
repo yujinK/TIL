18b1861d3e4c1a9e195c0eebb0ca1dfffdebda8e
yujinK <yujinkim.dev@gmail.com>
2019-05-07 18:10:28 +0900

    프린터

--- Algorithm/Programmers/Printer.java
+++ Algorithm/Programmers/Printer.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 public class Printer {
 
 	public static void main(String[] args) {
-		int[] priorities = {1, 1, 9, 1, 1, 1};
+		int[] priorities = {1, 1, 9, 1, 1, 1}; 
 		int location = 0;
 		System.out.println(solution(priorities, location));
 	}
