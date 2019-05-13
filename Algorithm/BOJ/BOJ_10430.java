eb76abd80e949a93236157656acade5d0f777c7b
yujinK <yujinkim.dev@gmail.com>
2019-05-13 17:17:57 +0900

    나머지

---
+++ Algorithm/BOJ/BOJ_10430.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_10430 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		System.out.println((a + b) % c);
+		System.out.println((a % c + b % c) % c);
+		System.out.println((a * b) % c);
+		System.out.println((a % c * b % c) % c);
+		scan.close();
+	}
+
+}
