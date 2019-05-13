5ec1d7883ce62536b03d617f472b4889eed4efe8
yujinK <yujinkim.dev@gmail.com>
2019-05-13 17:17:31 +0900

    A/B

---
+++ Algorithm/BOJ/BOJ_1008.java
@@ -0,0 +1,13 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_1008 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(scan.nextDouble() / scan.nextDouble());
+		scan.close();
+	}
+
+}
