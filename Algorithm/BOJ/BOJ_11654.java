2f8c32e451dcbc073693d43d2bc65daa99e54253
yujinK <yujinkim.dev@gmail.com>
2019-05-15 10:31:31 +0900

    아스키 코드

---
+++ Algorithm/BOJ/BOJ_11654.java
@@ -0,0 +1,18 @@
+package boj;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class BOJ_11654 {
+
+	public static void main(String[] args) throws IOException {
+//		Scanner scan = new Scanner(System.in);
+//		char c = scan.next().charAt(0);
+//		System.out.printf("%d", (int) c);
+//		scan.close();
+		
+		int x = System.in.read();
+		System.out.println(x);
+	}
+
+}
