a65ef8fbd6a41fcad353f21ef624a3ed30f17356
yujinK <yujinkim.dev@gmail.com>
2019-07-04 19:04:50 +0900

    190704 제일 작은 수 제거하기

--- Algorithm/Programmers/RemoveMin.java
+++ Algorithm/Programmers/RemoveMin.java
@@ -10,28 +10,34 @@ public class RemoveMin {
 	}
 	
 	public static int[] solution(int[] arr) {
-		int[] answer = new int[arr.length-1];
-		int min = 0;
-		int index = 0;
-		
-		if (answer.length == 0) {
-			return new int[] {-1};
-		}
-		
-		for (int i=1; i<arr.length; i++) {
-			if (arr[i] < arr[min]) {
-				min = i;
-			}
-		}
-		
-		for (int i=0; i<arr.length; i++) {
-			if (i != min) {
-				answer[index] = arr[i];
-				index++;
-			}
-		}
-		
-		return answer;
+		if (arr.length == 1) return new int[] {-1};
+		int min = Arrays.stream(arr).min().getAsInt();
+		return Arrays.stream(arr).filter(i -> i != min).toArray();
 	}
+	
+//	public static int[] solution(int[] arr) {
+//		int[] answer = new int[arr.length-1];
+//		int min = 0;
+//		int index = 0;
+//		
+//		if (answer.length == 0) {
+//			return new int[] {-1};
+//		}
+//		
+//		for (int i=1; i<arr.length; i++) {
+//			if (arr[i] < arr[min]) {
+//				min = i;
+//			}
+//		}
+//		
+//		for (int i=0; i<arr.length; i++) {
+//			if (i != min) {
+//				answer[index] = arr[i];
+//				index++;
+//			}
+//		}
+//		
+//		return answer;
+//	}
 
 }
