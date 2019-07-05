e8270faab68daae025c8facc9c505f20d6b3b173
yujinK <yujinkim.dev@gmail.com>
2019-07-06 00:36:02 +0900

    190706 정수 내림차순으로 배치하기

--- Algorithm/Programmers/SortDesc.java
+++ Algorithm/Programmers/SortDesc.java
@@ -3,21 +3,28 @@ package programmers;
 import java.util.Arrays;
 
 public class SortDesc {
-
+	static String res;
+	
 	public static void main(String[] args) {
 		System.out.println(solution(7123456789L));
 	}
 	
 	public static long solution(long n) {
-		char[] c = String.valueOf(n).toCharArray();
-		Arrays.sort(c);
-		String answer = "";
-		
-		for (int i=c.length-1; i>=0; i--) {
-			answer += c[i];
-		}
-		
-		return Long.parseLong(answer);
+		res = "";
+		String.valueOf(n).chars().sorted().forEach(c -> res = Character.valueOf((char) c) + res);
+		return Long.parseLong(res);
 	}
+	
+//	public static long solution(long n) {
+//		char[] c = String.valueOf(n).toCharArray();
+//		Arrays.sort(c);
+//		String answer = "";
+//		
+//		for (int i=c.length-1; i>=0; i--) {
+//			answer += c[i];
+//		}
+//		
+//		return Long.parseLong(answer);
+//	}
 
 }
