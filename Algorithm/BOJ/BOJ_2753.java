1bf8e2118326eb27b935bd77758ea73537dc0026
yujinK <yujinkim.dev@gmail.com>
2019-07-08 19:41:36 +0900

    190708 윤년

---
+++ Algorithm/BOJ/BOJ_2753.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_2753 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int year = scan.nextInt();
+		
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+		
+		scan.close();
+	}
+
+}
