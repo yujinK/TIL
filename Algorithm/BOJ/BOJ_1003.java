1d0cacd18f23d8231294a6e4e8575deb53dc45ed
yujinK <yujinkim.dev@gmail.com>
2019-08-08 22:58:00 +0900

    190808 피보나치 함수

---
+++ Algorithm/BOJ/BOJ_1003.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ_1003 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[][] fibo = new int[41][2];
+		fibo[0][0] = 1;
+		fibo[0][1] = 0;
+		fibo[1][0] = 0;
+		fibo[1][1] = 1;
+		
+		for (int i=2; i<=40; i++) {
+			fibo[i][0] = fibo[i-1][0] + fibo[i-2][0];
+			fibo[i][1] = fibo[i-1][1] + fibo[i-2][1];
+		}
+		
+		int t = scan.nextInt();
+		
+		for (int i=0; i<t; i++) {
+			int n = scan.nextInt();
+			System.out.println(fibo[n][0] + " " + fibo[n][1]);
+		}
+		
+		scan.close();
+	}
+}
