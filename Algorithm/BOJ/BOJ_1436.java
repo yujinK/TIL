5603f7f937ff39e2404f5d0b821ce40bf5e2186b
yujinK <yujinkim.dev@gmail.com>
2019-08-09 20:42:02 +0900

    190809 영화감독 숌

--- Algorithm/BOJ/BOJ_1436.java
+++ Algorithm/BOJ/BOJ_1436.java
@@ -3,25 +3,53 @@ package boj;
 import java.util.Scanner;
 
 public class BOJ_1436 {
-
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int count = 0;
-		int result = 0;
-		int num = 666;
+		int count = 0, i = 666, temp = 0;
 		
-		while (count != n) {
-			if (String.valueOf(num).contains("666")) {
-				count++;
-				result = num;
+		while (true) {
+			temp = i;
+			
+			while (temp >= 666) {
+				if (temp % 10 == 6 && (temp / 10 % 10 == 6) && (temp / 10 / 10 % 10 == 6)) {
+					count++;
+					break;
+				}
+				
+				temp /= 10;
 			}
 			
-			num++;
+			if (count == n) {
+				System.out.println(i);
+				break;
+			}
+			
+			i++;
 		}
 		
-		System.out.println(result);
 		scan.close();
 	}
+
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		int n = scan.nextInt();
+//		int count = 0;
+//		int result = 0;
+//		int num = 666;
+//		
+//		while (count != n) {
+//			if (String.valueOf(num).contains("666")) {
+//				count++;
+//				result = num;
+//			}
+//			
+//			num++;
+//		}
+//		
+//		System.out.println(result);
+//		scan.close();
+//	}
 
 }
