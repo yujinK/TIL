9823b680d7a9ef685bf19e5e3881501231e384f5
yujinK <yujinkim.dev@gmail.com>
2019-10-10 10:23:24 +0900

    191010 스택수열

--- Algorithm/BOJ/BOJ_1874.java
+++ Algorithm/BOJ/BOJ_1874.java
@@ -1,8 +1,6 @@
 package boj;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class BOJ_1874 {
 
@@ -36,42 +34,4 @@ public class BOJ_1874 {
 		System.out.println(sb.toString());
 	}
 
-//	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-//		Stack<Integer> stack = new Stack<>();
-//		String result = "";
-//		int n = scan.nextInt();
-//		int[] nums = new int[n];
-//		int now = 1;
-//		int index = 0;
-//
-//		for (int i=0; i<n; i++) {
-//			nums[i] = scan.nextInt();
-//		}
-//
-//		while (index < n) {
-//			if (!stack.isEmpty() && stack.peek() > nums[index]) {
-//				System.out.println("NO");
-//				return;
-//			}
-//			if (!stack.isEmpty() && stack.peek() == nums[index]) {
-//				stack.pop();
-//				index++;
-//				result += "-\n";
-//			} else {
-//				stack.push(now);
-//				now++;
-//				result += "+\n";
-//			}
-//		}
-//
-//		for (int i : stack) {
-//			result += "-\n";
-//		}
-//
-//		System.out.println(result);
-////		for (String s : output) {
-////			System.out.println(s);
-////		}
-//	}
 }
