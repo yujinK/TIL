6f4621fb472f094e35e31cfcdb3f21ef55b096e6
yujinK <yujinkim.dev@gmail.com>
2020-05-05 01:35:11 +0900

    200504 위장

--- Algorithm/Programmers/Camouflage.java
+++ Algorithm/Programmers/Camouflage.java
@@ -3,25 +3,27 @@ package programmers;
 import java.util.HashMap;
 
 public class Camouflage {
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (int i=0; i<clothes.length; i++) {
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 0) + 1);
+        }
 
-	public static void main(String[] args) {
-		String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
-		System.out.println(solution(clothes));
-	}
-	
-	public static int solution(String[][] clothes) {
-		int answer = 1;
-		HashMap<String, Integer> map = new HashMap<>();
-		
-		for (int i=0; i<clothes.length; i++) {
-			map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
-		}
-		
-		for (String s : map.keySet()) {
-			answer *= map.get(s) + 1;
-		}
-		
-		return answer - 1;
-	}
+        for (String key : hm.keySet()) {
+            int count = hm.get(key);
+            answer *= count + 1;
+        }
 
+        return answer - 1;
+    }
+
+    public static void main(String[] args) {
+        String[][] clothes = {
+                {"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"},
+                {"green_turban", "headgear"}
+        };
+        System.out.println(solution(clothes));
+    }
 }
