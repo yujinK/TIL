fe0017995361a9bc7bea2d243ed9cc777465c7bd
yujinK <yujinkim.dev@gmail.com>
2020-05-05 01:35:25 +0900

    200504 탑

--- Algorithm/Programmers/Tower.java
+++ Algorithm/Programmers/Tower.java
@@ -4,33 +4,29 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class Tower {
+    public static void main(String[] args) {
+        int[] heights = {6, 9, 5, 7, 4};
+        System.out.println(Arrays.toString(solution(heights)));
+    }
 
-	public static void main(String[] args) {
-		int[] heights = {3, 9, 9, 3, 5, 7, 2};
-		System.out.println(Arrays.toString(solution(heights)));
-	}
-	
-	public static int[] solution(int[] heights) {
-		int[] answer = new int[heights.length];
-		Stack<Integer> stack = new Stack<Integer>();
-		
-		for (int i=0; i<heights.length; i++) {
-			for (int j=0; j<i; j++) {
-				if (heights[j] > heights[i]) {
-					stack.push(j + 1);
-				}
-			}
-			
-			if (!stack.isEmpty()) {
-				answer[i] = stack.pop();
-			} else {
-				answer[i] = 0;
-			}
-			
-			stack.clear();
-		}
-		
-		return answer;
-	}
+    public static int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
 
+        Stack<Integer> stack = new Stack<>();
+        for (int i=0; i<heights.length; i++) {
+            for (int j=0; j<i; j++) {
+                if (heights[j] > heights[i]) {
+                    stack.push(j+1);
+                }
+            }
+
+            if (!stack.isEmpty()) {
+                answer[i] = stack.pop();
+            }
+
+            stack.clear();
+        }
+
+        return answer;
+    }
 }
