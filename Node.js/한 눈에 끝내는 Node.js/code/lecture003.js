31dc6b262bf67dae228fec564d45807463e2fe4c
yujinK <yujinkim.dev@gmail.com>
2020-09-12 01:23:52 +0900

    03. 객체지향 자바스크립트 공부중

--- Node.js/한 눈에 끝내는 Node.js/code/lecture003.js
+++ Node.js/한 눈에 끝내는 Node.js/code/lecture003.js
@@ -76,3 +76,68 @@ console.log(player.Pass());
 
 console.log(player.Sleep());
 // "Anonymous Soccer Player is slepping!"
+
+
+/**
+ * 생성자
+ */
+function SoccerPlayer(name, position) {
+    this.name = name;
+    this.position = position;
+    this.whatIsYourName = function() {
+        return "My name is " + this.name;
+    };
+    this.whatIsYourPosition = function() {
+        return "My position is " + this.position;
+    };
+}
+
+var player = new SoccerPlayer("Park Ji Sung", "Wing Forward");
+console.log(player.whatIsYourName());
+// "My name is Park Ji Sung"
+console.log(player.whatIsYourPosition());
+// "My position is Wing Forward"
+
+console.log(player.constructor);
+// Function: SoccerPlayer
+
+var player2 = new player.constructor("Koo Ja Cheol");
+console.log(player2.name);
+// "Koo Ja Cheol"
+
+
+console.log(player instanceof SoccerPlayer);
+// true
+
+console.log(player instanceof Object);
+// true
+
+
+var global_scope = 'global';	// 전역 스코프
+
+var local_function = function() {
+    var local_scope = 'goorm';	// 지역 스코프
+    console.log(global_scope);	// 전역 스코프 참조 가능
+    console.log(local_scope);	// 지역 스코프 참조 가능
+};
+
+console.log(local_scope);		// 지역 스코프이기 때문에 에러 발생
+
+
+var a = 1;
+
+function outer() {
+    var b = 2;
+    console.log(a);	//1
+    
+    function inner() {
+        var c = 3;
+        console.log(b);
+        console.log(a);
+    }
+    
+    inner();		// 2 1
+}
+outer();
+
+console.log(c);		// c is not defined
