33255bdc8b892ff5bbb39efd322defcf7484d339
yujinK <yujinkim.dev@gmail.com>
2020-09-16 00:05:40 +0900

    OS 모듈

--- Node.js/한 눈에 끝내는 Node.js/code/lecture006.js
+++ Node.js/한 눈에 끝내는 Node.js/code/lecture006.js
@@ -1,2 +1,15 @@
 console.log('process env property : ', process.env);
 console.log('uptime method : ', process.uptime());
+
+
+var os = require('os');
+console.log(os.tmpdir());
+console.log(os.type());
+
+var cpus = os.cpus();
+
+for (var i=0; i<cpus.length; i++) {
+    console.log("CPU[" + (i+1) + "]");
+    console.log("model: " + cpus[i].model);
+    console.log("speed: " + cpus[i].speed);
+}
