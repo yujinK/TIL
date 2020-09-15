709cbcbd8d3b4e92659ea7721fd00c5560a9a563
yujinK <yujinkim.dev@gmail.com>
2020-09-16 00:49:30 +0900

    06. 기본 모듈과 Node.js 기초 완료

--- Node.js/한 눈에 끝내는 Node.js/code/lecture006.js
+++ Node.js/한 눈에 끝내는 Node.js/code/lecture006.js
@@ -88,3 +88,27 @@ custom_event.on('call', function() {
 custom_event.removeAllListeners();
 
 custom_event.emit('call');
+
+
+function Foo() {
+    // 코드
+}
+
+Foo.prototype = {
+    bar: function() {
+        console.log('Foo_bar 실행');
+    }
+};
+
+function Bar() {
+}
+
+Bar.prototype = Object.create(Foo.prototype);
+
+Bar.prototype.baz = function() {
+    console.log('Bar_baz 실행');
+};
+
+Foo.prototype.bar();
+Bar.prototype.bar();
+Bar.prototype.baz();
