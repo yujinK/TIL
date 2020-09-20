e3778a674db8eeece4e7cd6d9fafeee84832f4dc
yujinK <yujinkim.dev@gmail.com>
2020-09-20 21:40:28 +0900

    200920 01타일

--- Algorithm/BOJ/BOJ_1904.java
+++ Algorithm/BOJ/BOJ_1904.java
@@ -1,22 +1,24 @@
 package boj;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class BOJ_1904 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n  = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		int[] tiles = new int[n+1];
-		tiles[1] = 1;
-		tiles[2] = 2;
-		
-		for (int i=3; i<=n; i++) {
-			tiles[i] = (tiles[i-1] % 15746) + (tiles[i-2] % 15746);
-		}
-		
-		System.out.println(tiles[n] % 15746);
-		scan.close();
-	}
+        if (n <= 2) {
+            System.out.println(n);
+        } else {
+            int[] tiles = new int[n+1];
+            tiles[1] = 1;
+            tiles[2] = 2;
 
+            for (int i = 3; i <= n; i++) {
+                tiles[i] = (tiles[i - 1] + tiles[i - 2]) % 15746;
+            }
+
+            System.out.println(tiles[n]);
+        }
+    }
 }
