f66efe36153c79c26156fad899f662c9429aeaa4
yujinK <yujinkim.dev@gmail.com>
2021-04-10 23:06:29 +0900

    1이 될 때까지(그리디) - p.99
    효율적 방법 적용

--- Algorithm/이것이 취업을 위한 코딩 테스트다/Part02/until_one.py
+++ Algorithm/이것이 취업을 위한 코딩 테스트다/Part02/until_one.py
@@ -10,3 +10,21 @@ while n > 1:
   result += 1
 
 print(result)
+
+
+# N이 K의 배수가 되도록 효율적으로 한 번에 빼는 방식
+# n, k = map(int, input().split())
+# result = 0
+
+# while True:
+#     target = (n // k) * K
+#     result += (n - target)
+#     n = target
+
+#     if n < k:
+#         break
+#     result += 1
+#     n //= k
+
+# result += (n - 1)
+# print(result)
