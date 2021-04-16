e54cb8c31be5a8dca228a092d5c580ca3919ab5d
yujinK <yujinkim.dev@gmail.com>
2021-04-16 23:41:20 +0900

    상하좌우 (구현) - p.110
    - 풀이적용

--- Algorithm/이것이 취업을 위한 코딩 테스트다/Part02/left_right_up_down.py
+++ Algorithm/이것이 취업을 위한 코딩 테스트다/Part02/left_right_up_down.py
@@ -1,25 +1,44 @@
+# n = int(input())
+# data = list(input().split())
+
+# x = 1
+# y = 1
+
+# for move in data:
+#     if move == 'R':
+#         y += 1
+#         if y > n:
+#             y = n
+#     elif move == 'L':
+#         y -= 1
+#         if y <= 0:
+#             y = 1
+#     elif move == 'U':
+#         x -= 1
+#         if x <= 0:
+#             x = 1
+#     elif move == 'D':
+#         x += 1
+#         if x > n:
+#             x = n
+
+# print(x, y)
+
 n = int(input())
-data = list(input().split())
+x, y = 1, 1
+plans = input().split()
 
-x = 1
-y = 1
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_type = ['L', 'R', 'U', 'D']
 
-for move in data:
-    if move == 'R':
-        y += 1
-        if y > n:
-            y = n
-    elif move == 'L':
-        y -= 1
-        if y <= 0:
-            y = 1
-    elif move == 'U':
-        x -= 1
-        if x <= 0:
-            x = 1
-    elif move == 'D':
-        x += 1
-        if x > n:
-            x = n
+for plan in plans:
+    for i in range(len(move_type)):
+        if plan == move_type[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
 
 print(x, y)
