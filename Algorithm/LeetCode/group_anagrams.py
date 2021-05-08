da6ac9b840601449de9887733797738eda6e8539
yujinK <yujinkim.dev@gmail.com>
2021-05-08 20:14:37 +0900

    210508 Group Anagrams

--- Algorithm/LeetCode/group_anagrams.py
+++ Algorithm/LeetCode/group_anagrams.py
@@ -6,8 +6,4 @@ anagrams = collections.defaultdict(list)
 for str in strs:
     anagrams[''.join(sorted(str))].append(str)
 
-result = []
-for value in anagrams.values():
-    result.append(value)
-
-print(result)
+print(list(anagrams.values()))
