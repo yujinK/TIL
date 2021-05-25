76a22fa6e47da5dcc298f0f30b1ef030cd2bac9b
yujinK <yujinkim.dev@gmail.com>
2021-05-25 21:59:17 +0900

    2120525 Two Sum
    - for문 개선

--- Algorithm/LeetCode/two_sum.py
+++ Algorithm/LeetCode/two_sum.py
@@ -3,9 +3,6 @@ target = 0
 
 nums_map = {}
 for i, num in enumerate(nums):
+    if target - num in nums_map:
+        print([nums_map[target - num], i])
     nums_map[num] = i
-
-for i, num in enumerate(nums):
-    if target - num in nums_map and i != nums_map[target - num]:
-        print([i, nums_map[target - num]])
-        break
