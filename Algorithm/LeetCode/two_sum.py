b72c8f0501307d522d8c06276431fd81b301a496
yujinK <yujinkim.dev@gmail.com>
2021-05-25 21:51:37 +0900

    210525 Two Sum
    - 파.알.인 풀이 적용

--- Algorithm/LeetCode/two_sum.py
+++ Algorithm/LeetCode/two_sum.py
@@ -1,16 +1,11 @@
 nums = [0, 4, 3, 0]
 target = 0
 
-result = []
-for i in range(0, len(nums) - 1):
-    flag = False
-    for j in range(i + 1, len(nums)):
-        if nums[i] + nums[j] == target:
-            result = [i, j]
-            flag = True
-            break
+nums_map = {}
+for i, num in enumerate(nums):
+    nums_map[num] = i
 
-    if flag:
+for i, num in enumerate(nums):
+    if target - num in nums_map and i != nums_map[target - num]:
+        print([i, nums_map[target - num]])
         break
-
-print(result)
