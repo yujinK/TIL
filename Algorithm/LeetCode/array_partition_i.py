51b84d55ce7ef6377a2d6fe69b9bf37227318668
yujinK <yujinkim.dev@gmail.com>
2021-05-29 21:54:41 +0900

    210529 Array Partition I
    - Pythonic Way

--- Algorithm/LeetCode/array_partition_i.py
+++ Algorithm/LeetCode/array_partition_i.py
@@ -1,8 +1,10 @@
 nums = [1, 4, 3, 2]
-nums.sort()
+# nums.sort()
 
-sum = 0
-for i in range(0, len(nums), 2):
-    sum += nums[i]
+# sum = 0
+# for i in range(0, len(nums), 2):
+#     sum += nums[i]
 
-print(sum)
+# print(sum)
+
+print(sum(sorted(nums)[::2]))
