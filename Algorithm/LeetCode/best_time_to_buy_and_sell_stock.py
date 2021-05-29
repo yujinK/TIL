f2db6445e24b0d76b19aaaf9e471749c25c8f12a
yujinK <yujinkim.dev@gmail.com>
2021-05-30 01:08:03 +0900

    210529 Best Time to Buy and Sell Stock
    - Kadane's Algorithm

--- Algorithm/LeetCode/best_time_to_buy_and_sell_stock.py
+++ Algorithm/LeetCode/best_time_to_buy_and_sell_stock.py
@@ -1,12 +1,12 @@
+import sys
+
 prices = [7, 1, 5, 3, 6, 4]
 
-min_value = prices[0]
-benefit = 0
+profit = 0
+min_price = sys.maxsize
 
-for i in range(1, len(prices)):
-    if (prices[i] - min_value) > benefit:
-        benefit = prices[i] - min_value
-    if prices[i] < min_value:
-        min_value = prices[i]
+for price in prices:
+    min_price = min(min_price, price)
+    profit = max(profit, price - min_price)
 
-print(benefit)
+print(profit)
