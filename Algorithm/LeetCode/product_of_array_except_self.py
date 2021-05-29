c69b2fc68543d72940fef2f02f79a610dcdd62c0
yujinK <yujinkim.dev@gmail.com>
2021-05-29 23:52:18 +0900

    210529 Product of Array Except Self
    - Runtime improvements

--- Algorithm/LeetCode/product_of_array_except_self.py
+++ Algorithm/LeetCode/product_of_array_except_self.py
@@ -2,25 +2,15 @@ import math
 
 nums = [1, 2, 3, 4]
 
-left_prod = []
-right_prod = []
+out = [1] * len(nums)
+p = 1
+for i in range(0, len(nums)):
+    out[i] = p
+    p = p * nums[i]
 
-for i in range(len(nums) - 1):
-    if i == 0:
-        left_prod.append(1)
-        left_prod.append(nums[i])
-    else:
-        left_prod.append(left_prod[i] * nums[i])
+p = 1
+for i in range(len(nums) - 1, -1, -1):
+    out[i] = out[i] * p
+    p = p * nums[i]
 
-for i in range(len(nums) - 1, 0, -1):
-    if i == len(nums) - 1:
-        right_prod.append(1)
-        right_prod.append(nums[i])
-    else:
-        right_prod.append(right_prod[len(nums) - i - 1] * nums[i])
-
-result = []
-for i in range(len(nums)):
-    result.append(left_prod[i] * right_prod[len(nums) - i - 1])
-
-print(result)
+print(out)
