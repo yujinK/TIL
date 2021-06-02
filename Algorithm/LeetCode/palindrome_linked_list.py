5ba53dce3a4852000b97b45967d1fbb5f26922eb
yujinK <yujinkim.dev@gmail.com>
2021-06-03 00:17:51 +0900

    210602 Palindrome Linked List
    - 데크 풀이

--- Algorithm/LeetCode/palindrome_linked_list.py
+++ Algorithm/LeetCode/palindrome_linked_list.py
@@ -1,19 +1,41 @@
+import collections
+from typing import Deque
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        value_list = []
-        while True:
-            value_list.append(head.val)
-            if head.next != None:
-                head = head.next
-            else:
-                break
+    # # 리스트 풀이
+    # def isPalindrome(self, head: ListNode) -> bool:
+    #     value_list = []
+    #     while True:
+    #         value_list.append(head.val)
+    #         if head.next != None:
+    #             head = head.next
+    #         else:
+    #             break
 
-        return value_list == value_list[::-1]
+    #     return value_list == value_list[::-1]
+
+    # 데크 풀이
+    def isPalindrome(self, head: ListNode) -> bool:
+        q: Deque = collections.deque()
+        
+        if not head:
+            return True
+        
+        node = head
+        while node is not None:
+            q.append(head.val)
+            node = head.next
+
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+        
+        return True
 
 solution = Solution()
 head = ListNode(1)
