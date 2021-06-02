ea249169e474a3bff18ddf3535b1d2ef1b978048
yujinK <yujinkim.dev@gmail.com>
2021-06-03 00:34:33 +0900

    210602 Palindrome Linked List
    - 런너 적용

--- Algorithm/LeetCode/palindrome_linked_list.py
+++ Algorithm/LeetCode/palindrome_linked_list.py
@@ -7,7 +7,7 @@ class ListNode:
         self.next = next
 
 class Solution:
-    # # 리스트 풀이
+    # # 1. 리스트 풀이
     # def isPalindrome(self, head: ListNode) -> bool:
     #     value_list = []
     #     while True:
@@ -19,23 +19,41 @@ class Solution:
 
     #     return value_list == value_list[::-1]
 
-    # 데크 풀이
-    def isPalindrome(self, head: ListNode) -> bool:
-        q: Deque = collections.deque()
+    # # 2. 데크 풀이
+    # def isPalindrome(self, head: ListNode) -> bool:
+    #     q: Deque = collections.deque()
         
-        if not head:
-            return True
+    #     if not head:
+    #         return True
         
-        node = head
-        while node is not None:
-            q.append(head.val)
-            node = head.next
+    #     node = head
+    #     while node is not None:
+    #         q.append(head.val)
+    #         node = head.next
 
-        while len(q) > 1:
-            if q.popleft() != q.pop():
-                return False
+    #     while len(q) > 1:
+    #         if q.popleft() != q.pop():
+    #             return False
         
-        return True
+    #     return True
+
+    # 3. 런너 풀이
+    def isPalindrome(self, head: ListNode) -> bool:
+        rev = None
+        slow = fast = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            rev, slow = rev.next, slow.next
+        
+        return not rev
+
 
 solution = Solution()
 head = ListNode(1)
@@ -47,4 +65,6 @@ head.next = node2
 node2.next = node3
 node3.next = node4
 
-print(solution.isPalindrome(head))
+# print(solution.isPalindrome(head))
+aa = None
+print(not aa)
