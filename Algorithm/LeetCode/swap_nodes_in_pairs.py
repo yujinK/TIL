fa4d6d7e9051df534b2f710fc0b2634d3cf6ed93
yujinK <yujinkim.dev@gmail.com>
2021-07-04 00:35:04 +0900

    210703 Swap Nodes in Pairs
    - 재귀 구조로 스왑

--- Algorithm/LeetCode/swap_nodes_in_pairs.py
+++ Algorithm/LeetCode/swap_nodes_in_pairs.py
@@ -26,21 +26,30 @@ class Solution:
             
     #     return result.next
 
+    # def swapPairs(self, head: ListNode) -> ListNode:
+    #     root = prev = ListNode(None)
+    #     prev.next = head
+
+    #     while head and head.next:
+    #         b = head.next
+    #         head.next = b.next
+    #         b.next = head
+
+    #         prev.next = b
+
+    #         head = head.next
+    #         prev = prev.next.next
+
+    #     return root.next
+
     def swapPairs(self, head: ListNode) -> ListNode:
-        root = prev = ListNode(None)
-        prev.next = head
-
-        while head and head.next:
-            b = head.next
-            head.next = b.next
-            b.next = head
-
-            prev.next = b
-
-            head = head.next
-            prev = prev.next.next
-
-        return root.next
+        if head and head.next:
+            p = head.next
+            head.next = self.swapPairs(p.next)
+            p.next = head
+            return p
+        
+        return head
 
 solution = Solution()
 head = ListNode(1)
